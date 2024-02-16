6260cc26b4c6181f90574beac099055ac5a6019e
Aleksiiej <aleksander.ramut@wp.pl>
2024-02-16 11:13:25 +0100

    .

--- app/main.cpp
+++ app/main.cpp
@@ -2,17 +2,11 @@
 
 #include <QApplication>
 
-#include "Database.hpp"
-#include "Menu.hpp"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    Database database;
-    Menu menu{database};
 
     return a.exec();
 }
