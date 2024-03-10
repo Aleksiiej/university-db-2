6c92e6e0c8d5a3269bae385e471bfdd46e256157
Aleksiiej <aleksander.ramut@wp.pl>
2024-03-10 21:14:25 +0100

    .

--- src/test/MenuTest.cpp
+++ src/test/MenuTest.cpp
@@ -1,10 +1,8 @@
-#include <gtest/gtest.h>
-#include "DatabaseMock.hpp"
 #include "Menu.hpp"
-
-class MenuFixture : public ::testing::Test
-{
+#include "DatabaseMock.hpp"
+#include <gtest/gtest.h>
+class MenuFixture : public ::testing::Test {
 public:
-    testing::NiceMock<DatabaseMock> db;
-    Menu menu{db};
+  testing::NiceMock<DatabaseMock> db;
+  Menu menu{db};
 };
